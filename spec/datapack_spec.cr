bc4eac882281673f0cb5332b03f6ec73786249bb
Kirk Haines <wyhaines@gmail.com>
2021-10-17 23:05:30 -0600

    Build out a few spec stubs to start TDDing this thing into existence.

--- spec/datapack_spec.cr
+++ spec/datapack_spec.cr
@@ -1,9 +1,19 @@
 require "./spec_helper"
+require "digest/sha256"
 
 describe Datapack do
-  # TODO: Write tests
+  it "can add a single file to the data store" do
+  end
 
-  it "works" do
-    false.should eq(true)
+  it "can add a full directory structure to the data store" do
+  end
+
+  it "can add files to different namespaces in the data store" do
+  end
+
+  it "can determine the mime types of some common file types" do
+  end
+
+  it "can fetch data from an http source" do
   end
 end
--- src/datapack.cr
+++ src/datapack.cr
@@ -1,6 +1,25 @@
-# TODO: Write documentation for `Datapack`
 module Datapack
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  struct Resource
+    getter type : String
+    getter data : String
+    getter path : Path
+
+    def initialize(path, @data)
+      @path = Path.new(path)
+      @type = File.extname(path).delete(".")
+    end
+  end
+
+  Data = Hash(Path, Resource).new
+
+  macro add_file(path, namespace = nil, mimetype = nil)
+    Datapack::Data[Path.new({{ path }})] = Datapack::Resource.new({{ path }},
+      {{ read_file(path) }})
+  end
+
+  def self.get(path)
+    Datapack::Data[Path.new(path)]
+  end
 end
